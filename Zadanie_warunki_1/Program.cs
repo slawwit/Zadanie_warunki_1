ed322f635b3526a84b43634eb392fc2b63bb00d5
Sławek Witek <slawekwitek@icloud.com>
2023-04-18 12:49:16 +0200

    Add project files.

---
+++ Zadanie_warunki_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadania_warunki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber;
+            int secondnumber;
+
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Podaj drugą liczbę: ");
+            string b = Console.ReadLine();
+            Int32.TryParse(a, out firstnumber);
+            Int32.TryParse(b, out secondnumber);
+
+            if (firstnumber == secondnumber)
+            {
+                Console.WriteLine($"Liczby {firstnumber} i {secondnumber} są równe.");
+            }
+            else
+            {
+                Console.WriteLine("Podane liczby nie są równe");
+            }
+        }
+    }
+}
